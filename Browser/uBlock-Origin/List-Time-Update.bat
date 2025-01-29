43c5c39e833bea3e2e3e6cfd6f80998b21dd5fa4
JPLiu <luticliu@foxmail.com>
2025-01-29 21:44:51 +0800

    20250129 214439 Add VersionTime

---
+++ Browser/uBlock-Origin/List-Time-Update.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set aria2=aria2c.exe
+set vim=vim.exe
+
+set hour=%time:~,2%
+if "%time:~,1%"==" " set hour=0%time:~1,1%
+set UpdateTime=%date:~0,4%%date:~5,2%%date:~8,2%%hour%%time:~3,2%
+
+for %%i in (*.list) do (
+    %vim% %%~ni.list -e -c "1,5s/Version: .*/Version: "%UpdateTime%"/ge | wq"
+)
+
+exit
--- Browser/uBlock-Origin/More-Rule-Upadte.bat
+++ Browser/uBlock-Origin/More-Rule-Upadte.bat
@@ -3,27 +3,22 @@
 set aria2=aria2c.exe
 set vim=vim.exe
 
-:: copy /y "%ubopath%"\my-ublock-backup.txt my-ublock-backup.txt
-
 del /s /q More-Rule-Temp\*
+
+set hour=%time:~,2%
+if "%time:~,1%"==" " set hour=0%time:~1,1%
+set UpdateTime=%date:~0,4%%date:~5,2%%date:~8,2%%hour%%time:~3,2%
 
 for %%i in (More-Rule/*.list) do (
     %aria2% --all-proxy 127.0.0.1:7890 --dir=More-Rule-Temp --input-file=More-Rule-UrlList\%%~ni.urllist
     type More-Rule-Temp\* >> %%~ni.listtemp
     del /s /q More-Rule-Temp\*
-    %vim% %%~ni.listtemp -e -c "source More-Rule-VimScript\%%~ni.vim | wq"
+    %vim% %%~ni.listtemp -e -c "source More-Rule-VimScript\%%~ni.vim | 1,5s/Version: .*/Version: "%UpdateTime%"/ge | wq"
 )
 
 move *.listtemp More-Rule\
-
-:: cd More-Rule\
-:: del *.list
-:: ren *.listtemp *.list
-:: copy /y *.list ..\..\AdGuard\*.txt
-
-:: cd ..
-:: copy /y *.list ..\AdGuard\*.txt
-:: del /s /q ..\AdGuard\Liu's-Rule-Group.txt
-:: del /s /q ..\AdGuard\Liu's-Trust-List.txt
+cd More-Rule\
+del *.list
+ren *.listtemp *.list
 
 exit
