5bbd8a51262bc4cf45c6d2ff6a94fc21c5bc43e2
BCZJG6GT <JPLiu@users.noreply.github.com>
2025-01-30 21:49:53 +0800

    Update More-Rule-Upadte.bat

--- Browser/uBlock-Origin/More-Rule-Upadte.bat
+++ Browser/uBlock-Origin/More-Rule-Upadte.bat
@@ -7,13 +7,13 @@ del /s /q More-Rule-Temp\*
 
 set hour=%time:~,2%
 if "%time:~,1%"==" " set hour=0%time:~1,1%
-set UpdateTime=%date:~0,4%%date:~5,2%%date:~8,2%%hour%%time:~3,2%
+set UpdateTime=%date:~0,4%%date:~5,2%%date:~8,2%%hour%%time:~3,2%%time:~6,2%
 
 for %%i in (More-Rule/*.list) do (
     %aria2% --all-proxy 127.0.0.1:7890 --dir=More-Rule-Temp --input-file=More-Rule-UrlList\%%~ni.urllist
     type More-Rule-Temp\* >> %%~ni.listtemp
     del /s /q More-Rule-Temp\*
-    %vim% %%~ni.listtemp -e -c "source More-Rule-VimScript\%%~ni.vim | 1,5s/Version: .*/Version: "%UpdateTime%"00/ge | wq"
+    %vim% %%~ni.listtemp -e -c "source More-Rule-VimScript\%%~ni.vim | 1,5s/Version: .*/Version: "%UpdateTime%"/ge | wq"
 )
 
 move *.listtemp More-Rule\
