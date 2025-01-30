222f2db9f4e48159e80b3c49ee2e9e1457c85343
BCZJG6GT <JPLiu@users.noreply.github.com>
2025-01-30 21:49:28 +0800

    Update List-Time-Update.bat

--- Browser/uBlock-Origin/List-Time-Update.bat
+++ Browser/uBlock-Origin/List-Time-Update.bat
@@ -5,10 +5,10 @@ set vim=vim.exe
 
 set hour=%time:~,2%
 if "%time:~,1%"==" " set hour=0%time:~1,1%
-set UpdateTime=%date:~0,4%%date:~5,2%%date:~8,2%%hour%%time:~3,2%
+set UpdateTime=%date:~0,4%%date:~5,2%%date:~8,2%%hour%%time:~3,2%%time:~6,2%
 
 for %%i in (*.list) do (
-    %vim% %%~ni.list -e -c "1,5s/Version: .*/Version: "%UpdateTime%"00/ge | wq"
+    %vim% %%~ni.list -e -c "1,5s/Version: .*/Version: "%UpdateTime%"/ge | wq"
 )
 
 exit
