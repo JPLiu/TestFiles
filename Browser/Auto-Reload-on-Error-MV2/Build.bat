95836fa3c48e377b67d09d12f9fa5e1cbbb93b7a
JPLiu <luticliu@foxmail.com>
2025-03-01 08:55:09 +0800

    20250301 085509

--- Browser/Auto-Reload-on-Error-MV2/Build.bat
+++ Browser/Auto-Reload-on-Error-MV2/Build.bat
@@ -2,14 +2,10 @@
 
 set zipper=D:\2-Extension\Installer\7-Zip\7za.exe
 
-del /s /q Auto-Reload-on-Error.xpi
+del /s /q Auto-Reload-on-Error.zip
 
 cd SourceCode
 
 "%zipper%" a -r -mx=0 -mcu=on "..\Auto-Reload-on-Error.zip" "*"
-
-cd ..
-
-ren Auto-Reload-on-Error.zip Auto-Reload-on-Error.xpi
 
 :: vim: set expandtab foldmethod=marker softtabstop=4 shiftwidth=4:
