331d43500caf82856fc02497b63405086de4e35e
JPLiu <luticliu@foxmail.com>
2025-03-12 21:02:23 +0800

    周三 2025/03/12 21:02:14.44

--- TestFilesReCommit.bat
+++ TestFilesReCommit.bat
@@ -1,9 +1,7 @@
 chcp 65001
 @echo off
 
-set hour=%time:~,2%
-if "%time:~,1%"==" " set hour=0%time:~1,1%
-set UpdateTime=%date:~0,4%%date:~5,2%%date:~8,2% %hour%%time:~3,2%%time:~6,2%
+set UpdateTime=%date% %time%
 
 git checkout --orphan newBranch
 git add -A
