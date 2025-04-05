807d19634bae71a0cc1bb027ac3a83f8ff0b8fc4
JPLiu <luticliu@foxmail.com>
2025-04-05 13:05:54 +0800

    周六 2025/04/05 13:05:53.56

--- Browser/uBlock-Origin/List-Time-Update.bat
+++ Browser/uBlock-Origin/List-Time-Update.bat
@@ -6,7 +6,7 @@ set vim=vim.exe
 
 set hour=%time:~,2%
 if "%time:~,1%"==" " set hour=0%time:~1,1%
-set UpdateTime=%date:~0,4%%date:~5,2%%date:~8,2%%hour%%time:~3,2%%time:~6,2%
+set UpdateTime=%date:~3,4%%date:~8,2%%date:~11,2%%hour%%time:~3,2%%time:~6,2%
 
 for %%i in (*.list) do (
     %vim% %%~ni.list -e -c "1,5s/Version: .*/Version: "%UpdateTime%"/ge | wq"
