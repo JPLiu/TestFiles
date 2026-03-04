64db2044b3a5f3cd32419516c673c363e13399a5
BCZJG6GT <JPLiu@users.noreply.github.com>
2026-03-04 09:12:44 +0800

    Refactor TestFilesRuleUpdate.bat script structure

--- TestFilesRuleUpdate.bat
+++ TestFilesRuleUpdate.bat
@@ -2,28 +2,22 @@ chcp 65001 >nul
 @echo off
 
 pushd Aria2
-
 pushd Aria2Conf
 start "" cmd /k call More-Rule-Upadte.bat
 popd
-
 popd
 
 pushd Android
-
 pushd BitTorrent
 start "" cmd /k call More-Rule-Upadte.bat
 popd
-
 popd
 
 pushd Browser
-
 pushd uBlock-Origin
 :: start "" cmd /k call More-Rule-Upadte.bat
 start "" cmd /k call List-Time-Update.bat
 popd
-
 popd
 
 :: vim: set expandtab foldmethod=marker softtabstop=4 shiftwidth=4:
